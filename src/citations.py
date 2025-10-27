8ae2da9fbce0f01bcfba32d1074f5159ae7bc231
fab <delgadoalpizarf@gmail.com>
2025-10-26 23:41:52 -0400

    [NF] Module to cite the paper

---
+++ src/citations.py
@@ -0,0 +1,9 @@
+# citations.py
+
+CITATIONS = {
+
+    "DEFAULT": [
+        "Guerra-Barroso, A.; Delgado-Alpízar F. J. and Varandas, A. J. C. pyCBS:\n"
+        "   A Complete Basis Set Extrapolation Tool. SoftwareX Year, Pages."
+    ]
+}
