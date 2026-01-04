34dc214efcdeeec61180a7fd404d7115298307c9
fab <delgadoalpizarf@gmail.com>
2026-01-04 15:01:50 -0500

    Extrapolation methodologies directory

--- src/methodologies/varandas/USTE2.py
+++ src/methodologies/varandas/USTE2.py
@@ -1,7 +1,6 @@
 import math
 from basis import hf, dc1, dc2, dc3  # Import dictionaries form basis.py
 
-# Function to retrieve dictionaries based on the selected method and two chosen databases
 def dictionaries(metodo, basis1, basis2, basis3, basis4):
     """"Return Hartree-Fock and correlation dictionaries based on the selected method and bases."""
     if metodo == "MP2":
