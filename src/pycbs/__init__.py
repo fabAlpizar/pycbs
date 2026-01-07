2d46d244f82c6850a79cd2c137bd21ce841bc16e
fab <delgadoalpizarf@gmail.com>
2026-01-07 05:22:03 -0500

    runner init to properly load every helper module

--- src/pycbs/__init__.py
+++ src/pycbs/__init__.py
@@ -1,16 +1,34 @@
+"""
+pyCBS - Complete Basis Set Extrapolation Tool
+
+Main package initialization
+"""
 import sys
 
+__all__ = [
+    'writer',
+    'basis',
+    'SchemeModuleLoader',
+    'ConfigValidator',
+    '__version__',
+]
+
+# Handle version
 if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+    from importlib.metadata import PackageNotFoundError, version  # pragma:  no cover
 else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version  # pragma:  no cover
 
 try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "pyCBS"
+    dist_name = "pycbs"
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+
+__author__ = "Alberto Guerra-Barroso, Fabio J. Delgado-Alpízar, Antonio J. C. Varandas"
+
+# Import key modules (relative imports within package)
+from .import writer
+from .import basis
+from .module_loader import SchemeModuleLoader
+from .config_validator import ConfigValidator
