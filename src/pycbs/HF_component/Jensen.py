3ad51b509e1bd5975955a5e23d3607e33249eccb
fab <delgadoalpizarf@gmail.com>
2026-01-07 00:16:23 -0500

    [WP] Jensen cbs extrapolation for HF component

--- src/pycbs/HF_component/Jensen.py
+++ src/pycbs/HF_component/Jensen.py
@@ -1,5 +1,5 @@
 import math as mt
-def Feller_HF_extrapolation(Ehf_X, Ehf_Y, X, Y, alfa=5.163):
+def Jensen_HF_extrapolation(Ehf_X, Ehf_Y, X, Y, alfa=5.163):
     num = Ehf_Y * (X+1) * mt.exp(-alfa * mt.sqrt(X) ) - Ehf_X * (Y+1) * mt.exp(-alfa * mt.sqrt(Y) )
     den = (X+1) * mt.exp(-alfa * mt.sqrt(X) ) - (Y+1) * mt.exp(-alfa * mt.sqrt(Y) )
     return num/den
