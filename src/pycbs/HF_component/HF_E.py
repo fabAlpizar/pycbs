d8bfccc054dfc6ab4450c46282135332f0257eb5
fab <delgadoalpizarf@gmail.com>
2026-01-07 01:05:13 -0500

    [WP] HF_E cbs extrapolation for HF component

--- src/pycbs/HF_component/HF_E.py
+++ src/pycbs/HF_component/HF_E.py
@@ -1,6 +1,5 @@
 import math
-from src.basis import hf, dc1, dc2, dc3
-
+from src.basis import hf
 def hartree_fock_energy(HF1, HF2, basis1, basis2):
     """Calculate the static correlation."""
     EHF = (HF1 * math.exp(2.284 * hf[basis1]) - HF2 * math.exp(2.284 * hf[basis2])) / \
